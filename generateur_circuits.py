b32b31a640ac1804c91b2ca4413af7fc2d8e149d
ighirane2004 <ighirane2004@gmail.com>
2026-07-28 17:14:51 +0100

    Update generateur_circuits.py

--- generateur_circuits.py
+++ generateur_circuits.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from google import genai
+from google.genai import types
 
 # Configuration de la page
 st.set_page_config(page_title="Générateur Circuitikz | Ighirane", page_icon="⚡", layout="wide")
